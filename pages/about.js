aa0ce048422af7b9762c86e29bf64b0f9a6344cc
kiran <kiran@neostack.com>
2017-04-22 08:09:52 +0530

    Multiple pages

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <div>Next.js is minimalistic framework for server-side rendering apps</div>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 import React from 'react'
 
 export default function Home() {
-  return <div>Welcome to Next.js</div>
+  return (
+    <div>
+      <h1>Next.js</h1>
+      <div>Welcome to Next.js</div>
+    </div>
+  )
 }
